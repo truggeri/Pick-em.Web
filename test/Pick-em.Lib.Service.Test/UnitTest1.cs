e6475f182f760cb9946e0ad6b14887ce2e538fe7
Thomas Ruggeri <truggeri@gmail.com>
2018-09-02 17:22:19 -0400

    Adding Service lib test project.

---
+++ test/Pick-em.Lib.Service.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Pick_em.Lib.Service.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
