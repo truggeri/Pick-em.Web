34f380829497a2103e0b580a60f7a7ae34806243
Thomas Ruggeri <truggeri@gmail.com>
2018-09-03 13:28:29 -0400

    [#10] Creating db connection extension method.

---
+++ src/Pick-em.Lib.Data/Extensions/DataConfigurationExtension.cs
@@ -0,0 +1,16 @@
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace Pick_em.Lib.Data.Extensions
+{
+    public static class DataConfigurationExtension
+    {
+        // http://www.npgsql.org/doc/index.html
+        public static void DataConfiguration(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSingleton<IDbConnection>(new NpgsqlConnection(config.GetConnectionString("Lib.Data")));
+        }
+    }
+}
