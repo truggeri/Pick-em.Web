27a1d8fa0780ce247a5528cba3159c601b4f78fa
Thomas Ruggeri <truggeri@gmail.com>
2018-09-03 21:21:03 -0400

    [#10] Adding health endpoint.

--- src/Pick-em.Web/Controllers/OkController.cs
+++ src/Pick-em.Web/Controllers/OkController.cs
@@ -1,13 +1,29 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
+using Pick_em.Lib.Data;
 
 namespace Pick_em.Web.Controllers
 {
     public class OkController : Controller
     {
+        private DatabaseUtils dbUtils { get; set; }
+
+        public OkController(DatabaseUtils givenUtils)
+        {
+            this.dbUtils = givenUtils;
+        }
+
         [HttpGet("/ok")]
         public IActionResult SimpleOk()
         {
             return Ok("ok");
+        }
+
+        [HttpGet("/health/db")]
+        public IActionResult DbHealth()
+        {
+            string health = this.dbUtils.HealthCheck();
+            return Ok($"{health}");
         }
     }
 }
