66692367a7367e76c150b36593b2a14476e39018
Thomas Ruggeri <truggeri@gmail.com>
2018-09-04 08:24:15 -0400

    Explicit Kestrel settings.

--- src/Pick-em.Web/Program.cs
+++ src/Pick-em.Web/Program.cs
@@ -7,7 +7,9 @@ namespace Pick_em.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
