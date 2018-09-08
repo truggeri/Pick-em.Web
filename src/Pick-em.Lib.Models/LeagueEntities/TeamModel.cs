7336007b3cb925bac1945164fb7bf7f13841cc31
Thomas Ruggeri <truggeri@gmail.com>
2018-09-08 11:29:50 -0400

    [#9] Adding League entities.

---
+++ src/Pick-em.Lib.Models/LeagueEntities/TeamModel.cs
@@ -0,0 +1,9 @@
+namespace Pick_em.Lib.Models
+{
+    public class TeamModel : BaseModel
+    {
+        public string Name;
+        public string PrimaryColor;
+        public LeagueModel League;
+    }
+}
