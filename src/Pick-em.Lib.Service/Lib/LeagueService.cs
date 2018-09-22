da8a603d25736dc19e68954c15501dc1f4edc8a7
Thomas Ruggeri <truggeri@gmail.com>
2018-09-22 19:23:07 -0400

    [#23] Adding logger to League service.

--- src/Pick-em.Lib.Service/Lib/LeagueService.cs
+++ src/Pick-em.Lib.Service/Lib/LeagueService.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 
 namespace Pick_em.Lib.Service
 {
     public class LeagueService
     {
+        private ILogger logger { get; }
+
+        public LeagueService
+        (
+            ILogger givenLogger
+        )
+        {
+            this.logger = givenLogger;
+        }
     }
 }
