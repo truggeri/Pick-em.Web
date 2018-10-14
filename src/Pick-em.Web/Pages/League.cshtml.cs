48f8f7baa787299adab17c8276eddd63d4c32f27
Thomas Ruggeri <truggeri@gmail.com>
2018-10-14 15:32:22 -0400

    [#27] Creating league page.

---
+++ src/Pick-em.Web/Pages/League.cshtml.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pick_em.Web.Pages
+{
+    public class LeagueModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
